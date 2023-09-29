6f95c03867c8cd922679cffd2f5b2a14ce6a9653
educatedgrape <educatedgrape@gmail.com>
2023-09-29 16:21:00 +0800

    Okay, so I'm going to try to make a commit

--- Home.py
+++ Home.py
@@ -6,8 +6,8 @@ import streamlit.components.v1 as components
 OpenAI_Key = st.secrets["OpenAI_Key"]
 
 st.set_page_config(
-    page_title="EduQuest",
-    page_icon="https://cdn-icons-png.flaticon.com/512/2617/2617909.png",
+    page_title="Grape Chat Bot",
+    page_icon="https://www.flaticon.com/free-icon/grapes_5246707?term=grape&page=1&position=15&origin=search&related_id=5246707,
     layout="wide",
     initial_sidebar_state="expanded",
 )
@@ -23,18 +23,7 @@ st.image("images/eduquest-logo2.png", width= 400)
 
 st.markdown("""
 
-            #### Welcome to EduQuest - a 6 in 1 education multitool.
-**By students, For Students**
-
-- **Text Summarizer** 📚🔍: Generates brief summaries from PDFs, articles, or input text, saving time on reading.
-            
-- **Video Summarizer** 🎥📝: Provides key points from YouTube videos via URL, eliminating the need to watch the whole video.
-            
-- **Semantic Search** 🔍📄: Uploading text enables quick keyword-based research for finding relevant information.
-            
-- **Notes and Questions** 📝❓: Generates well-structured notes and questions on input topics, aiding learning.
-            
-- **Chatbot** 🤖💬: Interact naturally with the Chatbot using files like .CSV or .TXT for quick answers.
+            Grape
 """)
 
             
